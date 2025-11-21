a2db2ea98790b627412dd7485c6f4615d473eace
Noorr4 <62324@students.riphah.edu.pk>
2025-11-22 02:33:21 +0500

    Updated snake.h and snake.cpp with new movement and grow logic

--- Snake.cpp
+++ Snake.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include "Snake.h"
+#include <vector>
 using namespace std;
 Snake::Snake(){
 	Node*newNode=new Node();//For Creating one block of snake in the center of grid
@@ -9,6 +10,10 @@ Snake::Snake(){
 	tail=newNode;
 	newNode->next=NULL;
 	length=1;
+}
+Node* Snake::gethead()
+{
+	return head;
 }
 void Snake::insertatend(int newX,int newY)
 {
@@ -63,26 +68,61 @@ void Snake::displaysnake()
 		temp=temp->next;
 	}
 }
-void Snake::movesnake(char direction) 
+int Snake::getheadX()
 {
-	//only moving the snake asnow it is not eating anything
-    int newX = head->x;
-    int newY = head->y;
+	return head->x;
+}
+int Snake::getheadY()
+{
+	return head->y;
+}
+void Snake::grow()
+{
+	// grow = add new block without removing tail
+    movesnake('X', true);  // We will NOT use 'X' direction; main will call grow after move
+}
 
-    if(direction == 'U') {
-        newY--;
+vector<Node*> Snake::getBody() {
+    vector<Node*> body;
+    Node* temp = head;
+    while (temp != NULL) {
+        body.push_back(temp);
+        temp = temp->next;
     }
-    else if(direction == 'D') {
-        newY++;
-    }
-    else if(direction == 'L') {
-        newX--;
-    }
-    else if(direction == 'R') {
-        newX++;
-    }
-
-    insertatend(newX, newY);
-    deleteattail();
+    return body;
+}
+void Snake::movesnake(char direction)
+{
+    movesnake(direction, false);   
+}
+void Snake::movesnake(char direction,bool grow)
+{
+	int newX=head->x;
+	int newY=head->y;
+	
+	if(direction=='U')
+	{
+		newY--;
+	}
+	else if(direction=='D')
+	{
+		newY++;
+	}
+	else if(direction=='L')
+	{
+		newX--;
+	}
+	else if(direction=='R')
+	{
+		newX++;
+	}
+	// create new head node (we use insertatend to add a block at tail side of your linked list)
+    // but your design uses head at front and tail at end; keeping your functions:
+    insertatend(newX,newY);
+    if(!grow)
+    {
+    	deleteattail();
+	}
+	
 }
 
--- Snake.h
+++ Snake.h
@@ -1,18 +1,35 @@
-struct Node{
-	Node *next;
-	int x;
-	int y;
+#ifndef SNAKE_H
+#define SNAKE_H
+#include <vector>
+using namespace std;
+struct Node {
+    int x;
+    int y;
+    Node *next;
 };
-class Snake{
-	private:
-		Node *head; //front of snake
-		Node *tail; //tail of the snake
-		int length; //counter
-	
-	public:
-		Snake();
-		void insertatend(int newX,int newY);
-		void deleteattail();
-		void movesnake(char direction);
-		void displaysnake();
+
+class Snake {
+    private:
+        Node *head;
+        Node *tail;
+        int length;
+
+    public:
+        Node* gethead();
+        int getheadX();
+        int getheadY();
+        Snake();
+        void insertatend(int newX,int newY);
+        void deleteattail();
+        void movesnake(char direction);
+        void displaysnake();
+        void movesnake(char direction,bool grow);
+        
+        // Utility for display & collision
+        vector<Node*>getBody();
+        
+        void grow();
 };
+
+#endif
+
